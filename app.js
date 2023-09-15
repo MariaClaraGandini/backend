c827a7aa5c74c2f9f03a0d4bebbfa5954d7dcae0
MariaClaraGandini <79529901+MariaClaraGandini@users.noreply.github.com>
2023-09-15 08:05:50 -0300

    alteracoes

--- app.js
+++ app.js
@@ -3,8 +3,15 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bcrypt = require ('bcrypt')
 const jwt = require ('jsonwebtoken')
+const cors = require('cors'); // Importe a biblioteca 'cors
+
+mongoose.set('strictQuery', false); // Configura a opção strictQuery como false
+
+
 
 const app = express()
+app.use(cors());
+
  app.use(express.json())
 
 const User = require('./models/User')
@@ -14,7 +21,7 @@ app.get ('/', (req, res) =>{
 })
 
 app.post('/auth/register', async(req,res) =>{
-	const{name, email, password, confirmpassword} = req.body
+	const{name, email, phone, state, city, password, confirmpassword} = req.body
 
 
 	///VALIDAÇÃO
@@ -25,19 +32,30 @@ app.post('/auth/register', async(req,res) =>{
 	if(!email){
 		return res.status(422).json({msg: 'Nome é obrigatório'})
 	}
+	if(!phone){
+		return res.status(422).json({msg: 'Telefone é obrigatório'})
+	}
+	if(!state){
+		return res.status(422).json({msg: 'Estado é obrigatório'})
+	}
+	if(!city){
+		return res.status(422).json({msg: 'Cidade é obrigatório'})
+	}
+
 	if(!password){
-		return res.status(422).json({msg: 'Nome é obrigatório'})
+		return res.status(422).json({msg: 'Senha é obrigatório'})
 	}
 	if(password !== confirmpassword){
-		return res.status(422).json({msg: 'A senha nao confere'})
+		return res.status(422).json({msg: 'A senha não confere'})
 		}
 
 	//CHECAR SE O E-MAIL JÁ É DE UMA CONTA EXISTENTE
-	const userExists = await User.findOne({email:email})
+	const userExists = await User.findOne({email:email} || {phone:phone})
 
 		if(userExists){
 		return res.status(422).json({msg: 'usuario já existe'})
 		}
+
 
 	//CRIPTOGRAFIA DE SENHA
 	const salt = await bcrypt.genSalt(12)
@@ -154,7 +172,7 @@ const dbUser = process.env.DB_USER
 const dbpassword = process.env.DB_PASS;
 
 mongoose.connect(`mongodb+srv://${dbUser}:${dbpassword}@cluster0.g6l4y4r.mongodb.net/`).then(()=>{	
-	app.listen(3000)
+	app.listen(5000)
 	console.log("conectou o bd")
 }).catch((err) => console.log(err))
 
